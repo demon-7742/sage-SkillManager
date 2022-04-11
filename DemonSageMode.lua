1e023b99fcf69ab3421d7ebe6a0d6c68b71c8718
demon-7742 <69797569+demon-7742@users.noreply.github.com>
2022-04-10 23:29:23 -0700

    Add files via upload

---
+++ DemonSageMode.lua
@@ -0,0 +1,165 @@
+-- Persistent Data
+local multiRefObjects = {
+
+} -- multiRefObjects
+local obj1 = {
+	["classes"] = {
+		[1] = false;
+		[2] = false;
+		[3] = false;
+		[4] = false;
+		[5] = false;
+		[6] = false;
+		[7] = false;
+		[8] = false;
+		[9] = false;
+		[10] = false;
+		[11] = false;
+		[12] = false;
+		[13] = false;
+		[14] = false;
+		[15] = false;
+		[16] = false;
+		[17] = false;
+		[18] = false;
+		[19] = false;
+		[20] = false;
+		[21] = false;
+		[22] = false;
+		[23] = false;
+		[24] = false;
+		[25] = false;
+		[26] = false;
+		[27] = false;
+		[28] = false;
+		[29] = false;
+		[30] = false;
+		[31] = false;
+		[32] = false;
+		[33] = false;
+		[34] = false;
+		[35] = false;
+		[36] = false;
+		[37] = false;
+		[38] = false;
+		[39] = false;
+		[40] = true;
+	};
+	["filters"] = {
+		[1] = "";
+		[2] = "";
+		[3] = "";
+		[4] = "";
+		[5] = "";
+	};
+	["mingp"] = 0;
+	["priosystem"] = false;
+	["skills"] = {
+		[1] = {
+			["combat"] = "Any";
+			["id"] = 24315;
+			["levelmin"] = 82;
+			["maxRange"] = 0;
+			["name"] = "Dyskrasia II";
+			["pnbuff"] = "167";
+			["prio"] = 1;
+			["tarange"] = 5;
+			["tecount"] = 2;
+			["terange"] = 5;
+			["trg"] = "Player";
+		};
+		[2] = {
+			["combat"] = "Any";
+			["id"] = 24297;
+			["levelmin"] = 46;
+			["maxRange"] = 0;
+			["name"] = "Dyskrasia";
+			["pnbuff"] = "167";
+			["prio"] = 2;
+			["tarange"] = 5;
+			["tecount"] = 2;
+			["terange"] = 5;
+			["trg"] = "Player";
+		};
+		[3] = {
+			["combat"] = "Any";
+			["dobuff"] = true;
+			["gcd"] = "False";
+			["id"] = 24290;
+			["ignoremoving"] = true;
+			["levelmin"] = 30;
+			["maxRange"] = 0;
+			["name"] = "Eukrasia";
+			["pnbuff"] = "2606";
+			["prio"] = 3;
+			["ptrg"] = "Enemy";
+			["tecount2"] = 1;
+			["terange"] = 5;
+			["thpl"] = 10;
+			["tnbuff"] = "2616+2614+2615";
+			["tnbuffdura"] = 1;
+			["trg"] = "Player";
+			["trgself"] = true;
+		};
+		[4] = {
+			["id"] = 24312;
+			["levelmin"] = 82;
+			["maxRange"] = 25;
+			["name"] = "Dosis III";
+			["pnbuff"] = "167";
+			["prio"] = 4;
+		};
+		[5] = {
+			["id"] = 24306;
+			["levelmin"] = 72;
+			["maxRange"] = 25;
+			["name"] = "Dosis II";
+			["pnbuff"] = "167";
+			["prio"] = 5;
+		};
+		[6] = {
+			["id"] = 24283;
+			["levelmin"] = 1;
+			["maxRange"] = 25;
+			["name"] = "Dosis";
+			["pnbuff"] = "167";
+			["prio"] = 6;
+		};
+		[7] = {
+			["id"] = 24312;
+			["ignoremoving"] = true;
+			["levelmin"] = 82;
+			["maxRange"] = 25;
+			["name"] = "Eukrasian Dosis III";
+			["pbuff"] = "2606";
+			["pnbuff"] = "167";
+			["prio"] = 7;
+			["tnbuff"] = "2616+2614+2615";
+		};
+		[8] = {
+			["id"] = 24306;
+			["ignoremoving"] = true;
+			["levelmin"] = 72;
+			["maxRange"] = 25;
+			["name"] = "Eukrasian Dosis III";
+			["pbuff"] = "2606";
+			["pnbuff"] = "167";
+			["prio"] = 8;
+			["tnbuff"] = "2616+2614+2615";
+		};
+		[9] = {
+			["combat"] = "Any";
+			["id"] = 24283;
+			["ignoremoving"] = true;
+			["levelmin"] = 1;
+			["maxRange"] = 25;
+			["name"] = "Eukrasian Dosis";
+			["pbuff"] = "2606";
+			["pnbuff"] = "167";
+			["prio"] = 9;
+			["tnbuff"] = "2616+2614+2615";
+		};
+	};
+	["version"] = 3;
+}
+return obj1
